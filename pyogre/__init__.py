61dbf18fba09cf46dc01df8b66f8b835e200b6ee
boogie <huseyinbiyik@hotmail.com>
2018-11-03 19:52:38 +0300

    pep8, py3

--- pyogre/__init__.py
+++ pyogre/__init__.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import six
 from requests.auth import HTTPBasicAuth
 
 
@@ -11,27 +12,31 @@ class api(object):
             self.auth = HTTPBasicAuth(self.key, self.secret)
         else:
             self.auth = None
-        
+
     def __query(self, path, auth, method, **kwargs):
         if auth:
             auth = self.auth
             if auth is None:
                 raise Exception("%s path needs authentication" % path)
-        r = requests.request(method, 'https://tradeogre.com/api/v1' + path, auth=HTTPBasicAuth(self.key, self.secret), data=kwargs)
+        r = requests.request(method, 'https://tradeogre.com/api/v1' + path,
+                             auth=HTTPBasicAuth(self.key, self.secret), data=kwargs)
         return json.loads(r.text)
-    
+
     @staticmethod
     def __objectify(d):
-        # this method fixes broken json data types in reponse data ie: float values returning strings etc..
+        # this method fixes broken json data types in reponse data
+        # ie: float values returning strings etc..
         if isinstance(d, dict):
             ret = {}
-            for x, y in d.iteritems():
-                if isinstance(y, (str, unicode)) and (y.isdigit() or y.replace(".", "").isdigit()):
+            for x, y in six.iteritems(d):
+                if isinstance(y, six.string_types) and \
+                        (y.isdigit() or y.replace(".", "").isdigit()):
                     y = float(y)
-                if isinstance(x, (str, unicode)) and (x.isdigit() or x.replace(".", "").isdigit()):
+                if isinstance(x, six.string_types) and \
+                        (x.isdigit() or x.replace(".", "").isdigit()):
                     x = float(x)
                 ret[x] = y
-        elif isinstance(d, (str, unicode)):
+        elif isinstance(d, six.string_types):
             if d.lower() == "true":
                 ret = True
             elif d.lower() == "false":
@@ -41,40 +46,41 @@ class api(object):
         else:
             ret = d
         return ret
-        
+
     @staticmethod
     def __stringify(d):
         if isinstance(d, (int, float)):
             return "%.8f" % d
-        
+
     def markets(self):
         markets = self.__query("/markets", False, "get")
-        return [{x: self.__objectify(y) for x, y in z.iteritems()} for z in markets]
-        
-        
+        return [{x: self.__objectify(y) for x, y in six.iteritems(z)} for z in markets]
+
     def orders(self, market):
         orders = self.__query("/orders/%s" % market, False, "get")
         for k in ["sell", "buy"]:
-            if not k in orders or not len(orders[k]):
+            if k not in orders or not len(orders[k]):
                 orders[k] = {}
-        return {x: self.__objectify(y) for x, y in orders.iteritems()}
-        
+        return {x: self.__objectify(y) for x, y in six.iteritems(orders)}
+
     def ticker(self, market):
         return self.__objectify(self.__query("/ticker/%s" % market, False, "get"))
 
     def history(self, market):
         return [self.__objectify(x) for x in self.__query("/history/%s" % market, False, "get")]
-        
+
     def buyorder(self, market, quantity, price):
         quantity = self.__stringify(quantity)
         price = self.__stringify(price)
-        return self.__objectify(self.__query("/order/buy", True, "post", market=market, quantity=quantity, price=price))
-    
+        return self.__objectify(self.__query("/order/buy", True, "post", market=market,
+                                             quantity=quantity, price=price))
+
     def sellorder(self, market, quantity, price):
         quantity = self.__stringify(quantity)
         price = self.__stringify(price)
-        return self.__objectify(self.__query("/order/sell", True, "post", market=market, quantity=quantity, price=price))
-    
+        return self.__objectify(self.__query("/order/sell", True, "post", market=market,
+                                             quantity=quantity, price=price))
+
     def cancelorder(self, uuid):
         return self.__query("/order/cancel", True, "post", uuid=uuid)
 
@@ -83,11 +89,12 @@ class api(object):
             kwargs = {"market": market}
         else:
             kwargs = {}
-        return [self.__objectify(x) for x in self.__query("/account/orders", True, "post", **kwargs)]
-        
+        return [self.__objectify(x) for x in self.__query("/account/orders", True,
+                                                          "post", **kwargs)]
+
     def myorder(self, uuid):
         return self.__objectify(self.__query("/account/order/%s" % uuid, True, "get"))
-        
+
     def balance(self, currency):
         return self.__objectify(self.__query("/account/balance", True, "post", currency=currency))
 
